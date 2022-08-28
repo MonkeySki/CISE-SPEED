1cf5163eea484346b50bf9096606a7778ec7b88a
MonkeySki <rebeccafredericknz@gmail.com>
2022-08-29 11:14:11 +1200

    Frontend on Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('../config/db');
 
+// // routes
+// const books = require('./routes/api/books');
+
 const app = express();
+const port = process.env.PORT || 8080;
+
+// cors config - allow same origin
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+// init cors
+app.use(cors(corsOptions));
+
+// init body parser
+app.use(express.json());
+
+// // use routes here
+// //app.use('/api/repo', repo);
+// app.use('/api/books', books);
 
 // Connect Database
 connectDB();
-
-app.get('/', (req, res) => res.send('Hello world! I want to deploy'));
-
-const port = process.env.PORT || 8082;
+if (process.env.NODE_ENV === 'production') {
+  // serve front-end client from build folder
+  app.use(express.static(__dirname+'/frontend/build'));
+  app.get('*', (req, res) =>{
+    res.sendFile(__dirname+'/frontend/build/index.html')
+  });
+  
+} else {
+  app.get('*', (req, res) => res.send(`API running on port ${port}`));
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
