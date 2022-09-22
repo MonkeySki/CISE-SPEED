72db826dd38bffa98ed31096a4eafb1ccadb72d4
MonkeySki <rebeccafredericknz@gmail.com>
2022-09-22 21:09:55 +1200

    remove local host to see if can fetch

--- frontend/src/components/articleList.js
+++ frontend/src/components/articleList.js
@@ -25,7 +25,7 @@ export default function ArticleList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getArticles() {
-     const response = await fetch(`http://localhost:5000/article/`);
+     const response = await fetch(`/article/`);
  
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
