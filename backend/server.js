c56c045cc133a094abb8320fadb33b69c4f5f0a1
MonkeySki <rebeccafredericknz@gmail.com>
2022-09-22 13:37:16 +1200

    server file order config first

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config({ path: "../config.env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const dbo = require("./db/conn");
-require("dotenv").config({ path: "../config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
