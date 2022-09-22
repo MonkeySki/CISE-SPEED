461ef3d8fc9cd8bf899ded54af36bacf655efb98
MonkeySki <rebeccafredericknz@gmail.com>
2022-09-22 16:32:38 +1200

    Database works locally, make new branch for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+require("dotenv").config({ path: "../config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-app.use(require("./routes/api/article"));
 console.log(`Before connection`);
+app.use(require("./routes/api/article"));
+
 // get driver connection
 const dbo = require("./db/conn");
 if (process.env.NODE_ENV === 'production') {
