8c16eaa33a7942b83cf92e1da8030f4accf74f25
MonkeySki <rebeccafredericknz@gmail.com>
2022-09-22 13:42:06 +1200

    Original Config Back

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
-require("dotenv").config({ path: "../config.env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const dbo = require("./db/conn");
+require("dotenv").config({ path: "../config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/article"));
 // get driver connection
-
+const dbo = require("./db/conn");
  
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -18,8 +17,5 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 
-  app.use(express.static(__dirname+'/../frontend/build'));
-  app.get('*', (req, res) =>{
-    res.sendFile(__dirname+'/../frontend/build/index.html')
-  });
+  
 });
