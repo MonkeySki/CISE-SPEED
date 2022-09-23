b3652139f86089a81740e953cd30eb6d32916deb
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-09-23 15:31:40 +1200

    Main branch resolved issue

--- backend/controller/articleController.js
+++ backend/controller/articleController.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
-const Article = require("../models/article");
-const Post = require("../models/post")
+// const Article = require("../models/article");
+const Posts = require("../models/post")
 
 //article creation
 
 module.exports = {
 
-    createArticle: (req,res) =>{
-        let newArticleDetails = req.body;
-        newArticleDetails._id =new mongoose.Types.ObjectId();
-        let article = new Article(newArticleDetails);
-        article.save((err)=>{
-            if(err){
-                return res.status(400).json(err);
-            }
-            res.json(article);
-        });
-    },
+    // createArticle: (req,res) =>{
+    //     let newArticleDetails = req.body;
+    //     newArticleDetails._id =new mongoose.Types.ObjectId();
+    //     let article = new Article(newArticleDetails);
+    //     article.save((err)=>{
+    //         if(err){
+    //             return res.status(400).json(err);
+    //         }
+    //         res.json(article);
+    //     });
+    // },
 
-    test:(req,res,next) =>{
-        Post.find((error, data) => {
-            if (error) {
-              return next(error)
-            } else {
-              //Transform card data into json and set as res
-              res.json(data)
-            }
-        // res.send('Testing that this works')
-    })
-    },
+    testFind:((req,res) =>{
+        //Finds all cards
+        Posts.find((error, data) => {
+        if (error) {
+          return next(error)
+        } else {
+          //Transform card data into json and set as res
+          res.json(data)
+        }
+        //Sort by count in descending order with a limit of 8
+      })
+    }),
 
     testPost:(req,res) =>{
     
         var name = {name:"jhon"};
-        let post = new Post(name);
+        let post = new Posts(name);
         console.log(post)
         console.log("Clear?")
         post.save((err)=>{
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,39 @@
 // app.js
 const express = require("express");
 const connectDB = require("../config/Db");
-var cors = require("cors");
+const cors = require("cors");
 
 // routes
 const post = require("./controller/articleController");
 
+// Connect Database
+connectDB();
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-const corsOptions = {
-  origin: true,
-  credentials: true,
-};
 
 // init cors
-app.use(cors(corsOptions));
+app.use(cors());
 
 // init body parser
 app.use(express.json());
 
 // use Routes
-app.get("/test", post.test);
+app.get("/test", post.testFind);
 app.post("/test", post.testPost);
 
-// Connect Database
-connectDB();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "/frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(__dirname + "frontend/build/index.html");
-  });
-} else {
-  app.get("*", (req, res) => res.send(`API running on port ${port}`));
-}
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(__dirname + "/frontend/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(__dirname + "frontend/build/index.html");
+//   });
+// } else {
+//   app.get("*", (req, res) => res.send(`API running on port ${port}`));
+// }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
