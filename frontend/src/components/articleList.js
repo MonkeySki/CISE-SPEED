64bb059b48ecdf8fc17869d7d9f8f75430c0889b
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-09-26 12:41:26 +1300

    Transfer from bootstrap to material ui with searchable datatable

--- frontend/src/components/articleList.js
+++ frontend/src/components/articleList.js
@@ -1,70 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { DataGrid } from "@mui/x-data-grid";
 
-const Article = (props) => (
-  <tr>
-    <td>{props.article.title}</td>
-    <td>{props.article.author}</td>
-    <td>{props.article.year}</td>
-    <td>{props.article.volume}</td>
-    <td>{props.article.number}</td>
-    <td>{props.article.pages}</td>
-    <td>{props.article.doi}</td>
-    <td>
-      <Link className="btn btn-link" to={`/edit/${props.article._id}`}>
-        Edit
-      </Link>{" "}
-      |
-      <button
-        className="btn btn-link"
-        onClick={() => {
-          props.deleteArticle(props.article._id);
-        }}
-      >
-        Delete
-      </button>
-    </td>
-  </tr>
-);
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
-  const rows = [
-    {
-      id: 1,
-      title: 1,
-      author: "Snow",
-      year: "Jon",
-      volume: 35,
-      number: 5,
-      pages: 4,
-      doi: 5,
-    },
-  ];
+  const rows =articles.map(({_id,title,author,year,volume,number,pages,doi})=>({id:_id,title,author,year,volume,number,pages,doi}))
+  console.log("Rows:",rows)
 
   const columns = [
-    { field: "id", headerName: "ID", width: 50 },
-    { field: "title", headerName: "Title", width: 50 },
-    { field: "author", headerName: "Author", width: 50 },
-    { field: "year", headerName: "Year", width: 50 },
-    { field: "volume", headerName: "Volume", width: 50 },
-    { field: "number", headerName: "Number", width: 50 },
-    { field: "pages", headerName: "Pages", width: 50 },
-    { field: "doi", headerName: "Doi", width: 50 },
+    { field: "title", headerName: "Title", width: 100 },
+    { field: "author", headerName: "Author", width: 100 },
+    { field: "year", headerName: "Year", width: 100 },
+    { field: "volume", headerName: "Volume", width: 100 },
+    { field: "number", headerName: "Number", width: 100 },
+    { field: "pages", headerName: "Pages", width: 100 },
+    { field: "doi", headerName: "Doi", width: 100 },
   ];
   // This method fetches the records from the database.
   useEffect(() => {
     async function getArticles() {
-      await axios.get("http://localhost:5000/article").then((res) => {
+      await axios.get("/article").then((res) => {
         console.log(res);
         if (!res.statusText === "OK") {
           console.log("checking for articles");
@@ -81,59 +38,26 @@ export default function ArticleList() {
     getArticles();
     return;
   }, [articles.length]);
+  
+  //ON CLICK HANDLE STUFF
+  const handleOnCellClick =(param)=>{
+    console.log("clicked:",param)
 
-  // This method will delete a record
-  async function deleteArticle(id) {
-    await fetch(`/${id}`, {
-      method: "DELETE",
-    });
-
-    const newArticles = articles.filter((el) => el._id !== id);
-    setArticles(newArticles);
-  }
-
-  // This method will map out the records on the table
-  function articleList() {
-    return articles.map((article) => {
-      console.log(rows);
-      return (
-        <Article
-          article={article}
-          deleteArticle={() => deleteArticle(article._id)}
-          key={article._id}
-        />
-      );
-    });
   }
 
   // This following section will display the table with the records of individuals.
   return (
     <div>
+      <h3>Article List</h3>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
+          onCellClick={handleOnCellClick}
         />
       </div>
-      <h3>Article List</h3>
-      <table className="table table-striped" style={{ marginTop: 20 }}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Journal</th>
-            <th>Year</th>
-            <th>volume</th>
-            <th>Number</th>
-            <th>Pages</th>
-            <th>DOI</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>{articleList()}</tbody>
-      </table>
     </div>
   );
 }
