8cfc504b97e9bd3822309cbcf1e2d5090877e775
MonkeySki <rebeccafredericknz@gmail.com>
2022-10-03 14:59:33 +1300

    feat: Basic Navbar

---
+++ frontend/src/components/analystList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AnalystList() {
+    return (
+        <>
+            <div>
+                <p>Analyst Hold Page For Bharath</p>
+            </div>
+        </>
+    )
+}
