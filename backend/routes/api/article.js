67e614d56eaf5eed2ed983f97d5f3eef108d8fd9
bharath12321 <102640553+bharath12321@users.noreply.github.com>
2022-10-16 21:37:36 +1300

    minor changes-nothing specific

--- backend/routes/api/article.js
+++ backend/routes/api/article.js
@@ -9,6 +9,8 @@ const article = require("../../model/article")
 
 // This will help us connect to the database
 const dbo = require("../../db/conn");
+const { json } = require("body-parser");
+const { response } = require("express");
 
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
@@ -26,64 +28,12 @@ articleRoutes.route("/article").get(function (req, res) {
     });
 });
 
-
-//analyst collection
-articleRoutes.route("/analyst").get(function (req, res){
-  let db_connect = dbo.getDb("cise");
-  db_connect
-  .collection("analyst")
-  .find({})
-  .toArray(function (err, result){
-    if(err) throw err;
-    res.json(result);
-  })
-})
-
-//rejected collection
-articleRoutes.route("/rejected").get(function (req, res) {
-  let db_connect = dbo.getDb("cise");
-  db_connect
-    .collection("rejected")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
-
-
 // This section will help you get a single record by id
 articleRoutes.route("/article/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
   db_connect
     .collection("articles")
-    .findOne(myquery, function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
-
-
-//analyst collection
-articleRoutes.route("/analyst/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect
-    .collection("analyst")
-    .findOne(myquery, function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
-
-
-//rejected collection
-articleRoutes.route("/rejected/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect
-    .collection("rejected")
     .findOne(myquery, function (err, result) {
       if (err) throw err;
       res.json(result);
@@ -107,35 +57,10 @@ articleRoutes.route("/article/add").post(function (req, response) {
 
   const evideince = new article(myobj);
   db_connect.collection("articles").insertOne(evideince, function (err, res) {
-    //console.log("HERE")
-    if (err) throw err;
-    response.json(res);
-  });
-});
-
-//rejected collection
-articleRoutes.route("/rejected").post(function (req, response) {
-  let db_connect = dbo.getDb("cise");
-  let myobj = {
-    title: req.body.title,
-    author: req.body.author,
-    journal: req.body.journal,
-    year: req.body.year,
-    volume: req.body.volume,
-    number: req.body.number,
-    pages: req.body.pages,
-    doi: req.body.doi,
-    claim: req.body.claim
-  };
-
-  const evideince = new article(myobj);
-  db_connect.collection("rejected").insertOne(evideince, function (err, res) {
     console.log("HERE")
     if (err) throw err;
     response.json(res);
   });
-
-  evideince.save();
 });
 
 // This section will help you update a record by id.
@@ -163,32 +88,6 @@ articleRoutes.route("/update/:id").post(function (req, response) {
   });
 });
 
-
-//rejected collection
-articleRoutes.route("/rejected/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    $set: {
-      title: req.body.title,
-      author: req.body.author,
-      journal: req.body.journal,
-      year: req.body.year,
-      volume: req.body.volume,
-      number: req.body.number,
-      pages: req.body.pages,
-      doi: req.body.doi,
-      claim: req.body.claim
-    },
-  };
-  db_connect.collection("rejected").updateOne(myquery, newvalues, function (err, res) {
-    if (err) throw err;
-    else
-      console.log(res);
-    response.json(res);
-  });
-});
-
 // This section will help you delete a record
 articleRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
@@ -201,15 +100,184 @@ articleRoutes.route("/:id").delete((req, response) => {
 });
 
 
-//analyst collection
-articleRoutes.route("/:id").delete((req, response) => {
+//////////////////////
+// MODERATOR ROUTES //
+//////////////////////
+
+articleRoutes.route("/moderator").get(function (req, res) {
+
+  let db_connect = dbo.getDb("cise");
+  db_connect
+    .collection("moderator")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single moderator record by id
+articleRoutes.route("/moderator/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("analyst").deleteOne(myquery, function (err, obj) {
+  db_connect
+    .collection("moderator")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you create a new moderator record.
+articleRoutes.route("/moderator/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    title: req.body.title,
+    author: req.body.author,
+    journal: req.body.journal,
+    year: req.body.year,
+    volume: req.body.volume,
+    number: req.body.number,
+    pages: req.body.pages,
+    doi: req.body.doi,
+    claim: req.body.claim,
+
+  };
+
+  const evidence = new article(myobj);
+  db_connect.collection("moderator").insertOne(evidence, function (err, res) {
     if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
+    response.json(res);
   });
 });
+
+articleRoutes.route('/moderator/delete/:id').delete(function (req, res) {
+  let db_connect = dbo.getDb("cise");
+
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("moderator")
+    .deleteOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+})
+  
+
+
+
+
+//////////////////////
+//  ANALYST ROUTES  //
+//////////////////////
+
+articleRoutes.route("/analyst").get(function (req, res) {
+  let db_connect = dbo.getDb("cise");
+  db_connect
+    .collection("analyst")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single analyst record by id
+articleRoutes.route("/analyst/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("analyst")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you create a new analyst record.
+articleRoutes.route("/analyst/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    title: req.body.title,
+    author: req.body.author,
+    journal: req.body.journal,
+    year: req.body.year,
+    volume: req.body.volume,
+    number: req.body.number,
+    pages: req.body.pages,
+    doi: req.body.doi,
+    claim: req.body.claim,
+
+  };
+
+  const evidence = new article(myobj);
+  db_connect.collection("analyst").insertOne(evidence, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+
+///////////////////////
+//  REJECTED ROUTES  //
+///////////////////////
+
+articleRoutes.route("/rejected").get(function (req, res) {
+  let db_connect = dbo.getDb("cise");
+  db_connect
+    .collection("rejected")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single moderator record by id
+articleRoutes.route("/rejected/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("moderator")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you create a new moderator record.
+articleRoutes.route("/rejected/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    title: req.body.title,
+    author: req.body.author,
+    journal: req.body.journal,
+    year: req.body.year,
+    volume: req.body.volume,
+    number: req.body.number,
+    pages: req.body.pages,
+    doi: req.body.doi,
+    claim: req.body.claim,
+
+  };
+
+  const evidence = new article(myobj);
+  db_connect.collection("rejected").insertOne(evidence, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+articleRoutes.delete('/rejected/delete/:id', (req, res) => {
+  var id = req.params.id;
+  db_connect.collection("moderator").deleteOne(id, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  }
+
+  );
+});
+
+
 
 module.exports = articleRoutes;
--- frontend/src/components/analystList.js
+++ frontend/src/components/analystList.js
@@ -224,10 +224,10 @@ export default function ArticleList() {
     console.log("clicked:", param);
   };
 
-  //function to delete row from datagrid -- not working
+  //function to delete row from datagrid
   const deleteRow = (clickedArticle) => {
-    setArticles(articles.filter((article) => article._id !== clickedArticle._id));
-    console.log(clickedArticle._id);
+    setArticles(articles.filter((article) => article.row._id !== clickedArticle.row._id));
+    console.log(clickedArticle.row._id);
   }
 
 
