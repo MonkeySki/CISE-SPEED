1bc743b1184ec9256a1b7880d03578fd5d0e41e0
Rebecca <90685844+MonkeySki@users.noreply.github.com>
2022-10-17 19:44:26 +1300

    Update moderatorList.js

--- frontend/src/components/moderatorList.js
+++ frontend/src/components/moderatorList.js
@@ -52,7 +52,7 @@ export default function ArticleList() {
     { field: "title", headerName: "Title", width: 100 },
     { field: "author", headerName: "Author", width: 100 },
     { field: "year", headerName: "Year", width: 100, filterOperators: quantityOnlyOperators },
-    { field: "volume", headerName: "Volume", widtWh: 100 },
+    { field: "volume", headerName: "Volume", width: 100 },
     { field: "number", headerName: "Number", width: 100 },
     { field: "pages", headerName: "Pages", width: 100 },
     { field: "doi", headerName: "Doi", width: 100 },
